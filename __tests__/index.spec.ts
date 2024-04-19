69d995d07560187602d37b8284a01f9d15e55f0f
yev wang <826036140@qq.com>
2024-04-19 12:26:59 +0800

    test: added unit tests

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -25,4 +25,17 @@ describe('Aquamark Basic Usage', () => {
     expect(latestEl.length).toEqual(1)
   })
 
+  test('Aquamark cannot be changed', async () => {
+    const mark = new Aquamark()
+    mark.init()
+    await sleep(500)
+    const el = document.getElementById(CANVAS_ID)!
+    el.style.opacity = '0'
+    await sleep(500)
+    const elNew = document.getElementById(CANVAS_ID)!
+    expect(elNew.style.opacity).toBe('')
+    expect(elNew).toMatchSnapshot()
+    expect(elNew.outerHTML).not.contain('oapcity')
+  })
+
 });
